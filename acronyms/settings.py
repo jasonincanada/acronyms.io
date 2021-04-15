b871202d97da01f712d8c8bea81ea838ed57f31f
jasonincanada <nirgle@gmail.com>
2021-04-15 19:54:33 -0400

    config for postgresql

--- acronyms/settings.py
+++ acronyms/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'acronyms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE':   'django.db.backends.postgresql',
+        'NAME':     'acro_dev',
+        'USER':     'acro_dev',
+        'PASSWORD': 'acro_dev',
+        'HOST':     'localhost',
+        'PORT':      5432
     }
 }
 
