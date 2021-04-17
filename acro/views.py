cded3e90af61d19faefd36bf0d1a5fb7ecb16844
jasonincanada <nirgle@gmail.com>
2021-04-17 17:20:41 -0400

    RoomView

--- acro/views.py
+++ acro/views.py
@@ -1,5 +1,17 @@
 from django.http      import HttpResponse
 from django.shortcuts import render
+from django.views     import generic
+
+from .models import Room
+
 
 def index(request):
   return HttpResponse("acro game main page")
+
+
+class RoomView(generic.DetailView):
+  model = Room
+  template_name = 'acro/room.html'
+  context_object_name = 'room'
+  slug_field = 'slug'
+
