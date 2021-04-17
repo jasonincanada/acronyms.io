6b15c9e9138785fa91098f435c81dc99ef260d37
jasonincanada <nirgle@gmail.com>
2021-04-17 15:23:00 -0400

    add custom User model

--- acro/models.py
+++ acro/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Custom user model:
+#   https://docs.djangoproject.com/en/3.2/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
+
+class User(AbstractUser):
+    pass
+
