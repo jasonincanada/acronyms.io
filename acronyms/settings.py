d3f0cde96f42de91d6c3b959e2d1626805d08d38
jasonincanada <nirgle@gmail.com>
2021-04-17 15:21:52 -0400

    set AUTH_USER_MODEL, add acro app

--- acronyms/settings.py
+++ acronyms/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['192.168.0.16']
 # Application definition
 
 INSTALLED_APPS = [
+    'acro.apps.AcroConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +128,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'acro.User'
+
