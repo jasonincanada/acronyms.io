c0b1258dc1bd957bcf1d78e644f78f4d9b7101a6
jasonincanada <nirgle@gmail.com>
2021-04-17 16:14:40 -0400

    models Game, Room

--- acro/models.py
+++ acro/models.py
@@ -7,3 +7,26 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
+
+class Room(models.Model):
+  slug = models.CharField(max_length=50)
+
+
+class Game(models.Model):
+  room     = models.ForeignKey(Room, on_delete=models.CASCADE)
+  acronym  = models.CharField(max_length=50)
+
+  # Game phases
+  GATHER   = 'G'
+  VOTE     = 'V'
+  FINISHED = 'F'
+
+  phases   = [ (GATHER,   "Gathering expansions")
+             , (VOTE,     "Voting on expansions")
+             , (FINISHED, "Game finished")
+             ]
+
+  phase    = models.CharField(max_length=1,
+                              choices=phases,
+                              default=GATHER)
+
