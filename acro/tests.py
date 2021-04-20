5f23a5cd0b32d572a84203ef6fa6264e94a8586b
jasonincanada <nirgle@gmail.com>
2021-04-20 12:42:19 -0400

    login tests

--- acro/tests.py
+++ acro/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import User
+
+# https://stackoverflow.com/questions/22457557/how-to-test-login-process/22463756#22463756
+class LoginTestCase(TestCase):
+  fixtures = ['basic.json']
+
+  def setUp(self):
+    self.credentials = {
+      'username': 'test',
+      'password': 'testtest123'
+    }
+
+  def test_valid_login(self):
+    response = self.client.post('/accounts/login/', self.credentials, follow=True)
+    self.assertTrue(response.context['user'].is_authenticated)
+
+  def test_invalid_login(self):
+    self.credentials.update({'password': 'wrongpassword'})
+    response = self.client.post('/accounts/login/', self.credentials, follow=True)
+    self.assertFalse(response.context['user'].is_authenticated)
+
