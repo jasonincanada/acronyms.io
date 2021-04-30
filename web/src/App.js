820f1d893d7e25409b90bd462cbf264ca1f63255
jasonincanada <nirgle@gmail.com>
2021-04-29 20:35:59 -0400

    add react router, set up basic routes

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,57 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        acronyms.io
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          acronyms.io
+        </header>
+
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/r/test">Test Room</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </nav>
+
+        <div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/r/:slug">
+            <Room />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+        </div>
+      </div>
+    </Router>
   );
+}
+
+const Home = () => {
+  return (
+    <div>This is the home page</div>
+  )
+}
+
+const Room = () => {
+  return (
+    <div>This is a room page</div>
+  )
+}
+
+const About = () => {
+  return (
+    <div>This is the about page</div>
+  )
 }
 
 export default App;
