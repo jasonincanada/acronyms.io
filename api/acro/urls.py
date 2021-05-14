eb04bc7d25b73dcbdd8ee8486bfa704ef32a43ce
jasonincanada <nirgle@gmail.com>
2021-05-14 00:54:01 +0000

    remove old django structure

--- api/acro/urls.py
+++ api/acro/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
 
 from . import api
-from . import views
 
 urlpatterns = [
-  path('',          views.HomeView.as_view(), name='home'),
-  path('r/<slug>/', views.RoomView.as_view(), name='room'),
-  # path('signup/',   views.SignUpView.as_view(), name='signup'),
 
+  # game api
   path('room/<slug>/get',                 api.get_room, name='api-get-room'),
   path('room/<int:room_id>/new-game/',    api.new_game, name='api-new-game'),
   path('activegame/<slug>/get',           api.get_activegame, name='api-get-activegame'),
@@ -17,6 +14,7 @@ urlpatterns = [
   path('phrase/<int:phrase_id>/vote/',    api.vote, name='api-vote'),
   path('votes/<int:game_id>/get',         api.get_votes, name='api-get-votes'),
 
+  # user auth
   path('csrf/get',                        api.get_csrf, name='api-get-csrf'),
   path('login',                           api.login_user, name='api-login-user'),
   path('signup',                          api.signup_user, name='api-signup-user'),
